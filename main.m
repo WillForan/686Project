1befde7d99de7d34e047ab576982682aa2300a63
Will Foran <wforan1@andrew.cmu.edu>
2011-04-24 15:20:20 -0400

    load in csv/mat files iteratively

--- main.m
+++ main.m
@@ -3,21 +3,30 @@ function main(dirpath)
 
     for f = 3:length(files) %skip . and ..
         dtry = files(f).name;
-	search=[dtry '/*mat'];
+	search=strcat(dirpath, dtry, '/*mat');
 	mat = dir(search);
 	e=[];
 	if(size(mat)==0)
-	    search=[dtry '/*csv'];
+	    return
+	    search=strcat(dirpath, dtry, '/*csv');
 	    csv = dir(search);
 	    e=importEmotiveData(csv(1).name);
 	    save([files(f).name,'.mat'],e) 
 	else
-	    load [files(f).name,'.mat'];
+	   matpath=strcat(dirpath,dtry,'/',mat.name)
+	   %load file
+	   load(matpath)
+	   %and loading does not bring in e
+	   if(~exist('e(1)','var'))
+	       %set it (via eval) to be e
+	       e=eval(mat.name(1:length(mat.name)-4));
+	   end
 	end
-	search=[dtry '/*pls'];
+	search=strcat(dirpath, dtry, '/*pls');
 	labfile=dir(search);
+        labpath=strcat(dirpath,dtry,'/',labfile(1).name);
 	labels=cell(1,1);
-	labels{1}=importdata(labfile(1).name);
+	labels{1}=importdata(labpath);
 
 	e=csCompile(e,labels,0)
 	csOSTest(e)

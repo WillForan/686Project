18031ed293f1880a351fa163712822b6fb1f8211
Will Foran <wforan1@andrew.cmu.edu>
2011-04-24 14:38:41 -0400

    main file

--- main.m
+++ main.m
@@ -1,10 +1,19 @@
-function main()
-    files = dir('./*CSV');
-    for f = 1:length(files),
-	str = files(f).name;
-	[subj str] = strtok(str, '-');
-	[cond str] = strtok(str, '-'); cond = find(strcmp(classes, cond));
-	[t str] = strtok(str, '-');    t = str2num(t);
+function main(dirpath)
+    files    = dir(dirpath);
+
+    for f = 3:length(files) %skip . and ..
+        dtry = files(f).name;
+	mat = dir([dtry,'/*mat']);
+	e=[];
+	if(size(mat)==0)
+	    csv = dir([dtry,'/*csv']);
+	    e=importEmotiveData(csv(1).name);
+	    save([files(f).name,'.mat'],e) 
+	else
+	    load [files(f).name,'.mat'];
+	end
+	labfile=dir([dtry,'/*pls']);
+	labels=importdata(labfile(1).name);
 
 	e=csCompile(e,labels,0)
 	csOSTest(e)
